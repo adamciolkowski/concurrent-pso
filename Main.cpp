4865673adb7d22611a5a31a8d14b4f9636720af7
Adam Ciolkowski <adamciolkowski1@gmail.com>
2015-11-26 21:35:48 +0100

    Execution time measurement

--- Main.cpp
+++ Main.cpp
@@ -17,7 +17,10 @@ int main() {
     SwarmParams params(0.2, 0.8, 0.8);
     PSO pso(100, 100, params);
 
+    clock_t start = clock();
     Solution *s = pso.optimize(problem);
+    double time = (clock() - start) / (double) CLOCKS_PER_SEC;
+    std::cout << "time= " << time << " s" << std::endl;
     cout << *s << endl;
     delete function;
 }
