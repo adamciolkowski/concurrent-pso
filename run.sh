fffc2b22767938664e6915835cc23aec52783fad
asidor <adriansidor1@gmail.com>
2015-11-30 15:23:21 +0100

    modified run script

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 g++ -std=c++11 -fopenmp Main.cpp SwarmParams.cpp PSO.cpp Particle.cpp Solution.cpp -o app
 ./app 1
+g++ -ftree-vectorize -std=c++11 -fopenmp Main.cpp SwarmParams.cpp PSO.cpp Particle.cpp Solution.cpp -o app
 ./app 2
-./app 3
 ./app 4
+./app 8
+./app 16
+./app 32
+./app 64
+
