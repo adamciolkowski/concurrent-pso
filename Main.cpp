05a3d1127c8e7d329db4688530b31bafd3d4a8f3
Adam Ciolkowski <adamciolkowski1@gmail.com>
2016-01-22 23:30:39 +0100

    Low-level implementation

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,110 @@
+#include <iostream>
+#include <random>
+#include <string.h>
+
+#define SWARM_SIZE 1000
+#define ITERATIONS 10000
+#define DIMENSIONS 10
+
+#define INERTIA_FACTOR 0.3
+#define GLOBAL_WEIGHT 0.8
+#define LOCAL_WEIGHT 0.8
+
+#define MAX_BOUND 5
+#define MIN_BOUND -5
+
+using namespace std;
+
+struct Particle {
+    double position[DIMENSIONS];
+    double velocity[DIMENSIONS];
+    double bestPosition[DIMENSIONS];
+};
+
+random_device rd;
+
+double randomInRange(double min, double max) {
+    uniform_real_distribution<double> unif(min, max);
+    default_random_engine re(rd());
+    return unif(re);
+}
+
+void printArray(const double array[], int length) {
+    cout << "[";
+    for (int i = 0; i < length - 1; ++i) {
+        cout << array[i] << ", ";
+    }
+    cout << array[length - 1] << "]" << endl;
+}
+
+static double constrict(double value, double min, double max) {
+    if (value < min)
+        return min;
+    if (value > max)
+        return max;
+    return value;
+}
+
+double fitness(const double x[]) {
+    double f = 0;
+    for (int i = 0; i < DIMENSIONS - 1; i++) {
+        f += 100 * pow(x[i + 1] - pow(x[i], 2), 2) + pow(x[i]-1, 2);
+    }
+    return f;
+}
 
 int main(int argc, char *argv[]) {
+    clock_t start = clock();
 
+    Particle particles[SWARM_SIZE];
+    for (int i = 0; i < SWARM_SIZE; ++i) {
+        struct Particle p;
+        for (int j = 0; j < DIMENSIONS; ++j) {
+            p.position[j] = randomInRange(MIN_BOUND, MAX_BOUND);
+        }
+        for (int j = 0; j < DIMENSIONS; ++j) {
+            p.velocity[j] = 0.0;
+        }
+        for (int j = 0; j < DIMENSIONS; ++j) {
+            memcpy(p.bestPosition, p.position, sizeof(double) * DIMENSIONS);
+        }
+        particles[i] = p;
+    }
+    double bestSwarmPosition[DIMENSIONS];
+    double bestFitness;
+    for (int i = 0; i < SWARM_SIZE; ++i) {
+        double f = fitness(particles[i].position);
+        if(i == 0 || f < bestFitness) {
+            memcpy(bestSwarmPosition, particles[i].position, sizeof(double) * DIMENSIONS);
+            bestFitness = f;
+        }
+    }
+
+    for (int i = 0; i < ITERATIONS; ++i) {
+        for (int j = 0; j < SWARM_SIZE; ++j) {
+            Particle &p = particles[j];
+            for (int k = 0; k < DIMENSIONS; ++k) {
+                double newPos = INERTIA_FACTOR * p.velocity[k] +
+                                (p.bestPosition[k] - p.position[k]) * LOCAL_WEIGHT +
+                                (bestSwarmPosition[k] - p.position[k]) * GLOBAL_WEIGHT;
+                p.velocity[k] = constrict(newPos, MIN_BOUND, MAX_BOUND);
+            }
+            for (int k = 0; k < DIMENSIONS; ++k) {
+                p.position[k] = p.position[k] * p.velocity[k];
+            }
+        }
+
+        for (int j = 0; j < SWARM_SIZE; ++j) {
+            double f = fitness(particles[j].position);
+            if(f < bestFitness) {
+                memcpy(bestSwarmPosition, particles[j].position, sizeof(double) * DIMENSIONS);
+                bestFitness = f;
+            }
+        }
+    }
+    double time = (clock() - start) / (double) CLOCKS_PER_SEC;
+
+    printArray(bestSwarmPosition, DIMENSIONS);
+    cout << "fitness = " << bestFitness << endl;
+    cout << "time    = " << time << " s" << endl;
 }
