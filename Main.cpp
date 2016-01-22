208f9647e6d43a623f3eeee6ea0b8ffbe8d31443
Adam Ciolkowski <adamciolkowski1@gmail.com>
2016-01-22 23:28:26 +0100

    Starting from scratch

--- Main.cpp
+++ Main.cpp
@@ -1,30 +1,4 @@
-#include <iostream>
-#include <openacc.h>
-#include "SwarmParams.h"
-#include "PSO.h"
-#include "RosenbrockFunction.h"
-
-using namespace std;
 
 int main(int argc, char *argv[]) {
-    //int n = atoi(argv[1]);
-    //omp_set_num_threads(n);
-    const int dimensions = 10;
-    ObjectiveFunction *function = new RosenbrockFunction(dimensions);
 
-    vector<double> lower(dimensions, -5);
-    vector<double> upper(dimensions, 5);
-    Bounds bounds(lower, upper);
-
-    OptimizationProblem problem(function, bounds);
-
-    SwarmParams params(0.2, 0.8, 0.8);
-    PSO pso(12000, 500, params);
-
-    //double startOmp = omp_get_wtime();
-    Solution *s = pso.optimize(problem);
-    //double time = omp_get_wtime() - startOmp;
-    //cout << "time= " << time << " s with " << n << " threads" << endl;
-    cout << *s << endl;
-    delete function;
 }
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-pgc++ -acc -std=c++11 -ta=nvidia -Minfo=accel Main.cpp SwarmParams.cpp PSO.cpp Particle.cpp Solution.cpp -o app
+pgc++ -acc -std=c++11 -ta=nvidia -Minfo=accel Main.cpp -o app
